e4cac7a367d2a96946c5bfcf82921109641e4858
h2yhm <heri2000@yahoo.com>
2023-07-26 22:45:40 +0700

    Sync with private repo

---
+++ src/MailSender.js
@@ -0,0 +1,34 @@
+const nodemailer = require('nodemailer');
+
+class MailSender {
+  constructor() {
+    console.log(`${process.env.SMTP_HOST} - ${process.env.SMTP_PORT} - ${process.env.SMTP_USER} - ${process.env.SMTP_PASSWORD}`);
+    this._transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    });
+  }
+
+  sendEmail(targetEmail, content) {
+    const message = {
+      from: 'Notes Apps',
+      to: targetEmail,
+      subject: 'Ekspor Catatan',
+      text: 'Terlampir hasil dari ekspor catatan',
+      attachments: [
+        {
+          filename: 'notes.json',
+          content,
+        },
+      ],
+    };
+
+    return this._transporter.sendMail(message);
+  }
+}
+
+module.exports = MailSender;
